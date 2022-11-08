45747b7aafdf745e080892082f45df0b7da9a2f5
Larisa-Mishura <mlarisa7777@gmail.com>
2022-11-08 19:12:37 +0200

    LJ3-104

--- src/main/java/com/mishura/MainFive.java
+++ src/main/java/com/mishura/MainFive.java
@@ -84,14 +84,13 @@ public class MainFive {
         int[] numbers = createArray(10, 0, 100);
         for (int j = 0; j < numbers.length-1; j++) {
             boolean isChanged = false;
-            for (int i = 0; i < numbers.length - 1; i++) {
+            for (int i = 0; i < numbers.length - j - 1; i++) {
                 if (numbers[i] > numbers[i + 1]) {
                     int temp = numbers[i];
                     numbers[i] = numbers[i + 1];
                     numbers[i + 1] = temp;
                     if (!isChanged){
-                        isChanged = true;
-                    }
+                        isChanged = true; }
                 }
             }
             if(!isChanged){
@@ -100,4 +99,5 @@ public class MainFive {
         }
         System.out.println(Arrays.toString(numbers));
     }
+
 }
