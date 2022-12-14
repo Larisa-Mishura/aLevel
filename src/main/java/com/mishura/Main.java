744a952047b57fe1a90f918cb7903ca5d9016a00
Larisa <mlarisa7777@gmail.com>
2022-12-14 16:25:33 +0200

    LJ3-252

--- src/main/java/com/mishura/Main.java
+++ src/main/java/com/mishura/Main.java
@@ -1,40 +1,26 @@
 package com.mishura;
 
 import com.mishura.model.Car;
+import com.mishura.model.PassengerCar;
 import com.mishura.model.Type;
 import com.mishura.repository.CarArrayRepository;
 import com.mishura.service.CarService;
+import com.mishura.util.AlgorithmUtil;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         final CarService carService = new CarService(new CarArrayRepository());
-        final Car car1 = carService.create(Type.CAR);
-        car1.restore();
-        final Car car2 = carService.create(Type.TRUCK);
-        car2.restore();
-        //System.out.println(carService.carEquals(car1, car2));
-        Car car3 = null;
+        carService.create(3, Type.CAR);
+        carService.create(3, Type.TRUCK);
+        carService.printAll();
+        Car[] cars = carService.getAll();
+        Car car1 = cars[1];
+        Car car2 = cars[2];
 
-        carService.printManufacturerAndCount(car1);
-        carService.printManufacturerAndCount(car2);
-        carService.printManufacturerAndCount(car3);
-
-        carService.printColor(car1);
-        carService.printColor(car2);
-        carService.printColor(car3);
-
-
-        carService.printEngineInfo(car1);
-        carService.printEngineInfo(car2);
-        carService.printEngineInfo(car3);
-
-        carService.printInfo(car1);
-        carService.printInfo(car2);
-        carService.printInfo(car3);
-
-        carService.checkCount(car1);
-        carService.checkCount(car2);
-        carService.checkCount(car3);
+        System.out.println(AlgorithmUtil.binarySearch(cars, car1));
+        System.out.println(AlgorithmUtil.binarySearch(cars, car2));
     }
 }
